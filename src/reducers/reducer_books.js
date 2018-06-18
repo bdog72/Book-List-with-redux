65393e896a8a80fd1acd45e4a85a7d7edf38d366
Brian Bycynski <high5bri@hotmail.com>
2018-06-18 08:28:11 -0400

    finished small app with redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    // eslint-disable-next-line
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 50 },
+    { title: 'The Dark Tower', pages: 25 },
+    { title: 'Eloquent Ruby', pages: 200 }
+  ];
+}
